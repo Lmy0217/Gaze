16c04b4ac5bddc87887437078e5895d6ac84e7d1
Lmy0217 <lmy0217@126.com>
2018-11-26 00:34:17 +0800

    add name

--- config.py
+++ config.py
@@ -4,11 +4,9 @@ import os
 class base(object):
 
     def __init__(self):
-
         self._space = 0
 
     def __eq__(self, other):
-
         if isinstance(other, self.__class__):
             flag = True
             for name, value in vars(self).items():
@@ -18,7 +16,6 @@ class base(object):
             return False
 
     def __repr__(self):
-
         s = []
         sp = []
         for i in range(self._space):
@@ -27,6 +24,10 @@ class base(object):
         if self._space == 0:
             s.extend(sp)
             s.append(self.__class__.__name__)
+            if 'name' in dir(self):
+                s.append(' (')
+                s.append(str(self.name))
+                s.append(') ')
             s.append(': ')
         s.append('{\n')
         sp2 = sp.copy()
@@ -36,7 +37,7 @@ class base(object):
         v.sort()
 
         for name, value in v:
-            if name in ['_space']:
+            if name in ['_space', 'name']:
                 continue
             s.extend(sp2)
             s.append(name)
@@ -61,7 +62,8 @@ __urls__ = [
 class paths(base):
 
     def __init__(self, data_folder='datasets', h5_file='gaze.h5', json_file='gaze.json', save_folder='save',
-                 check_file='_checkpoint.pth', ms_file='_ms.pth', logging_file='_logging.log'):
+                 check_file='_checkpoint.pth', ms_file='_ms.pth', predict_file='_predict.mat',
+                 logging_file='_logging.log'):
         super(paths, self).__init__()
         self.data_folder = data_folder
         self.h5_file = h5_file
@@ -69,15 +71,17 @@ class paths(base):
         self.save_folder = save_folder
         self.check_file = check_file
         self.ms_file = ms_file
+        self.predict_file = predict_file
         self.logging_file = logging_file
 
 
-class Dataset(base):
+class Config(base):
 
-    def __init__(self, batch_size, test_batch_size, epochs, lr, lr_decay, momentum, noise=0., data_ratio=0.5,
+    def __init__(self, name, batch_size, test_batch_size, epochs, lr, lr_decay, momentum, noise=0., data_ratio=0.5,
                  is3d=False, nonorm=False, nocuda=False, seed=1, ci=False, paths=paths(),
                  root_folder=os.path.abspath('.'), data_reset=False):
-        super(Dataset, self).__init__()
+        super(Config, self).__init__()
+        self.name = name
         self.batch_size = batch_size
         self.test_batch_size = test_batch_size
         self.epochs = epochs
@@ -97,116 +101,114 @@ class Dataset(base):
 
 
 def resnet_18():
-    dataset = Dataset(32, 32, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('resnet_18', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def resnet_34():
-    dataset = Dataset(96, 96, 30, 0.01, 0, 0)
-    return dataset
+    config = Config('resnet_34', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def resnet_50():
-    dataset = Dataset(32, 32, 30, 0.01, 0, 0)
-    return dataset
+    config = Config('resnet_50', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def resnet_101():
-    dataset = Dataset(16, 16, 30, 0.01, 0, 0)
-    return dataset
+    config = Config('resnet_101', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def resnet_152():
-    dataset = Dataset(16, 16, 30, 0.01, 0, 0)
-    return dataset
+    config = Config('resnet_152', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def cnn_7():
-    dataset = Dataset(32, 25000, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('cnn_7', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def cnn_12():
-    dataset = Dataset(32, 25000, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('cnn_12', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def capsule():
-    dataset = Dataset(32, 32, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('capsule', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def capsule_w():
-    dataset = Dataset(32, 32, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('capsule_w', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def capsule_7():
-    dataset = Dataset(256, 256, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('capsule_7', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def capsule_12():
-    dataset = Dataset(128, 128, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('capsule_12', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def capsule_3():
-    dataset = Dataset(32, 32, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('capsule_3', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def capsule_3w():
-    dataset = Dataset(192, 192, 6, 0.01, 0, 0)
-    return dataset
+    config = Config('capsule_3w', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def capsule_4():
-    dataset = Dataset(128, 128, 50, 0.01, 0, 0)
-    return dataset
+    config = Config('capsule_4', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def capsule_5():
-    dataset = Dataset(512, 512, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('capsule_5', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def squeezenet_v10():
-    dataset = Dataset(32, 1024, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('squeezenet_v10', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def squeezenet_v11():
-    dataset = Dataset(32, 1024, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('squeezenet_v11', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def fullscale():
-    dataset = Dataset(32, 32, 10, 0.01, 0, 0)
-    return dataset
+    config = Config('fullscale', 32, 32, 10, 0.01, 0, 0)
+    return config
 
 
 def cnn3d_3():
-    dataset = Dataset(1, 1, 50, 0.01, 0, 0, is3d=True)
-    return dataset
+    config = Config('cnn3d_3', 1, 1, 10, 0.01, 0, 0, is3d=True)
+    return config
 
 
 default = cnn_7()
 
 
 def cfg(model=None):
-    if model is not None:
-        return eval("" + model + "()")
-    else:
-        return default
+    return eval("" + model + "()") if model is not None else default
 
 
 def args2dataset(args):
-    dataset = Dataset(batch_size=args.batchsize, test_batch_size=args.testbatchsize, epochs=args.epochs, lr=args.lr,
-                      lr_decay=args.lrdecay, momentum=args.momentum, noise=args.noise, data_ratio=args.ratio,
-                      is3d=default.is3d, nonorm=default.nonorm, nocuda=args.no_cuda, seed=args.seed, ci=args.ci,
-                      paths=default.paths, root_folder=default.root_folder, data_reset=args.reset)
-    return dataset
+    config = Config(name=default.name, batch_size=args.batchsize, test_batch_size=args.testbatchsize,
+                    epochs=args.epochs, lr=args.lr, lr_decay=args.lrdecay, momentum=args.momentum, noise=args.noise,
+                    data_ratio=args.ratio, is3d=default.is3d, nonorm=default.nonorm, nocuda=args.no_cuda,
+                    seed=args.seed, ci=args.ci, paths=default.paths, root_folder=default.root_folder,
+                    data_reset=args.reset)
+    return config
 
 
 if __name__ == "__main__":
